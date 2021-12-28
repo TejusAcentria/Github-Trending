c2c47e753a0ae6ed3c2cc530be9fbc66f470eb4e
TejusCF <tejus.gupta2017@gmail.com>
2021-12-29 00:02:27 +0530

    remove some files

--- app/src/main/java/com/tj/githubapi/utils/Util.java
+++ app/src/main/java/com/tj/githubapi/utils/Util.java
@@ -8,46 +8,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import com.tj.githubapi.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Util {
-
-    public static int getYear(String mDate) {
-        Date date = null;
-        try {
-            date = new SimpleDateFormat("yyyy-MM-dd").parse(mDate);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return Integer.parseInt(new SimpleDateFormat("yyyy").format(date));
-    }
-
-    public static int getMonth(String mDate) {
-        Date date = null;
-        try {
-            date = new SimpleDateFormat("yyyy-MM-dd").parse(mDate);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return Integer.parseInt(new SimpleDateFormat("MM").format(date));
-    }
-
-    public static int getDay(String mDate) {
-        Date date = null;
-        try {
-            date = new SimpleDateFormat("yyyy-MM-dd").parse(mDate);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return Integer.parseInt(new SimpleDateFormat("dd").format(date));
-    }
-
     public static boolean isNetworkAvailable(Context context) {
         ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         // Test for connection
@@ -71,15 +37,6 @@ public class Util {
         textView.setTextColor(color);
         snackbar.show();
     }
-
-    public static String formatDate(int year, int month, int day) {
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.DAY_OF_MONTH, day);
-        cal.set(Calendar.MONTH, month-1);
-        return new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime());
-    }
-
     public static String getDefaultDate() {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date(System.currentTimeMillis()-24*60*60*1000));
     }
